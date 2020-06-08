f4d6da144b4096c0f0d6c4bab5eaa8c18a8f429c
kchohee <kchohee0630@gmail.com>
2020-06-08 14:12:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+    })
+    .then(function(json){
+      renderBooks(json)
+    })
+    // or 
+    // .then(resp => resp.json())
+  // .then(json => renderBooks(json))
 
 }
 
